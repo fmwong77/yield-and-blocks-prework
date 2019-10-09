452400cfd4e1e9c355c278a40fe17279db74f8dd
Mei Wong <fongmeiw@gmail.com>
2019-10-09 16:03:45 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -2,13 +2,13 @@ def hello_t(array)
   i = 0
  
   while i < array.length
-    yield(array[i])
+    yield(array[i]) if block_given?
     i = i + 1
   end
 end
 
 # call your method here!
-hello_t(array) {|name|
+hello_t {|name|
   if name.start_with?("T")
     puts "Hi, #{name}"
   end
